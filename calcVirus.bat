c1077823141ceed62f5eb16b80cbc39b990750af
C++ DEV <97477657+Javascript-rocks@users.noreply.github.com>
2022-12-13 13:04:18 +0000

    calc.bat

---
+++ calcVirus.bat
@@ -0,0 +1,119 @@
+@echo off
+title CALC V
+color 10
+
+echo """CALC VIRUS"""
+echo 1 + entr to start 
+
+set /p inp=
+
+if %inp%==1 goto x
+
+:x
+start calc.exe
+timeout 30
+start calc.exe
+start calc.exe
+timeout 30
+start calc.exe
+start calc.exe
+start calc.exe
+timeout 30
+start calc.exe
+start calc.exe
+start calc.exe
+timeout 30
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+timeout 30
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+timeout 30
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+timeout 30
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+
